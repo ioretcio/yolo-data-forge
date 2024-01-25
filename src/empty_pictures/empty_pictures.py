624b1483f61b6ce602a458aea8cff26168574865
ioretcio <goncharukyou@gmail.com>
2024-01-25 23:39:08 +0200

    refined empty pictures

--- src/empty_pictures/empty_pictures.py
+++ src/empty_pictures/empty_pictures.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-
+import time
 
 def dir_path(string):
     if os.path.isdir(string):
@@ -14,8 +14,7 @@ def empty_pictures():
         description='Finds images and labels without a pair in the image-labels directories.')
     parser.add_argument('--images', "-i", type=dir_path, help="images directory")
     parser.add_argument('--labels', "-l", type=dir_path, help="labels directory")
-    parser.add_argument('--symbol', "-s", type=str,
-                        help="character added to the beginning of the file name without a pair")
+    
     args = parser.parse_args()
 
     image_dir = args.images
@@ -24,41 +23,62 @@ def empty_pictures():
     images = os.listdir(image_dir)
     labels = os.listdir(label_dir)
 
-    labels_without_extension = []
-    images_without_extension = []
+    labels_dict = {}
+    images_dict = {}
 
-    symbol_change_to = '$'
-    
-    ending = ".jpg"
 
+    start_time = time.time()
+    print("Forming labels list")
     for label in labels:
-        if label.split('.')[-1] != "ini" and label.split('.')[-2] != "classes":
-            labels_without_extension.append(".".join(label.split('.')[:-1]))
-
+        if label.split('.')[-1] != "ini" and label.split('.')[-2] != "classes" and not "current_config" in label:
+            labels_dict[".".join(label.split('.')[:-1])] = label
+    print(f"Labels list formed in {time.time() - start_time} seconds")
+    
+    
+    
+    
+    start_time = time.time()
+    print("Forming images list")
     for image in images:
         if image.split('.')[-1] != "ini":
-            ending = image.split('.')[-1]
-            images_without_extension.append(".".join(image.split('.')[:-1]))
-
+            images_dict[".".join(image.split('.')[:-1])] = image
+    print(f"Images list formed in {time.time() - start_time} seconds")
+    
+    
+    
+    
+    start_time = time.time()
+    print("Processing images without pair")
     images_without_extension_count = 0
-    for image_without_extension in images_without_extension:
-        if image_without_extension[0] != '_' and  image_without_extension[0] != symbol_change_to:
-            if image_without_extension not in labels_without_extension:
-                images_without_extension_count += 1
-                os.rename(str(os.path.join(image_dir, image_without_extension + "." + ending)),
-                          str(os.path.join(image_dir, symbol_change_to + image_without_extension + "." + ending)))
-        else:
+    for image_without_extension in images_dict.keys():
+        if image_without_extension not in labels_dict.keys():
             images_without_extension_count += 1
-
+            
+            if not os.path.exists(os.path.join(image_dir, "single")):
+                os.mkdir(os.path.join(image_dir, "single"))
+                
+            os.rename(str(os.path.join(image_dir, images_dict[image_without_extension])),
+                        str(os.path.join(image_dir,"single", images_dict[image_without_extension])))
+    print(f"Images without labels moved in {time.time() - start_time} seconds")
+    
+    
+    start_time = time.time()
+    print("Processing images without pair")
     labels_without_extension_count = 0
-    for label_without_extension in labels_without_extension:
-        if label_without_extension[0] != '_' and label_without_extension[0] != symbol_change_to:
-            if label_without_extension not in images_without_extension:
-                labels_without_extension_count += 1
-                os.rename(str(os.path.join(label_dir, label_without_extension + ".txt")),
-                          str(os.path.join(label_dir, symbol_change_to + label_without_extension + ".txt")))
-        else:
+    for label_without_extension in labels_dict.keys():
+        if label_without_extension not in images_dict.keys():
             labels_without_extension_count += 1
-
+            
+            if not os.path.exists(os.path.join(label_dir, "single")):
+                os.mkdir(os.path.join(label_dir, "single"))
+                
+            os.rename(str(os.path.join(label_dir, labels_dict[label_without_extension])),
+                        str(os.path.join(label_dir,"single", labels_dict[label_without_extension])))
+    print(f"Labels without images moved in {time.time() - start_time} seconds")
+    print()
     print(f"{images_without_extension_count}-images without label!"
           f"\n{labels_without_extension_count}-labels without image!")
+    print(f'All result moved to {os.path.join(label_dir, "single")} and {os.path.join(image_dir, "single")} folders')
+
+
+empty_pictures()
