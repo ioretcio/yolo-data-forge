703d189a4def421af19a8318d7695beaa8fc804a
ioretcio <goncharukyou@gmail.com>
2024-03-21 17:42:28 +0200

    packer

--- src/packer/packer.py
+++ src/packer/packer.py
@@ -1,13 +1,14 @@
 import argparse
 import os
 import shutil
+import random
+
 
 def dir_path(string):
     if os.path.isdir(string): return string
     else: raise NotADirectoryError(string)
 
 extensions = ['jpg', 'png', 'JPG']
-zfil = 4
 
 def ends(file):
     for extension in extensions:
@@ -15,58 +16,48 @@ def ends(file):
             return True
     return False
 
-def main(images_folder,labels_folder, countInFolder):
-    images = []
-    for image in os.listdir(images_folder):
-        if os.path.isfile(os.path.join(images_folder,image)) and ends(image):
-            images.append( image  )
-    print(f"Images count {len(images)}")
-    
-    createdFolderIndex = 1
-    while(True):
-        print(f"Processing {createdFolderIndex} folder...")
-        
-        images_target_current = os.path.join(os.path.split(os.path.abspath(os.getcwd()))[-1]+str(createdFolderIndex).zfill(zfil), "images")
-        labels_target_current = os.path.join(os.path.split(os.path.abspath(os.getcwd()))[-1]+str(createdFolderIndex).zfill(zfil), "labels" )
-        
-        createdFolderIndex+=1
-        
-        if not os.path.exists( images_target_current):
-            os.makedirs(images_target_current)
-        if not os.path.exists( labels_target_current):
-            os.makedirs(labels_target_current)
-        
-        
-        
-        already=0
-        for image in os.listdir(images_target_current):
-            if os.path.isfile(os.path.join(images_target_current,image)) and ends(image):
-                already+=1
-        
-        for i in range(already, countInFolder):
+def main(images_folder,labels_folder, packs_count, packs_name):
+    siz = int(len(os.listdir(images_folder))/packs_count)
+
+    for i in range(1, packs_count + 1):
+        pack_folder = f'{packs_name}{i:04d}'
+        images_folder = os.path.join(pack_folder, 'images')
+        labels_folder = os.path.join(pack_folder, 'labels')
+        if not os.path.exists(pack_folder):
+            os.makedirs(pack_folder)
+        if not os.path.exists(images_folder):
+            os.makedirs(images_folder)
+        if not os.path.exists(labels_folder):
+            os.makedirs(labels_folder)
+
+    for i in range(1, packs_name + 1):
+        pack_folder = f'{packs_name}{i:04d}'
+        images_folder = os.path.join(pack_folder, 'images')
+        labels_folder = os.path.join(pack_folder, 'labels')
+        shutil.copy("classes.txt", labels_folder)
+        files = os.listdir(images_folder)
+        random.shuffle(files)
+        for file_name in files[:siz]:
+            file_path = os.path.join(images_folder, file_name)
+            destination_path = os.path.join(images_folder, file_name)
+            if not os.path.exists(destination_path):
+                shutil.move(file_path, destination_path)
+                label_file_name = os.path.splitext(file_name)[0] + '.txt'
+                label_file_path = os.path.join(labels_folder, label_file_name)
+                if os.path.exists(label_file_path):
+                    destination_label_path = os.path.join(labels_folder, label_file_name)
+                    shutil.move(label_file_path, destination_label_path)
             
-            shutil.move (  os.path.join(images_folder, images[0]), os.path.join( images_target_current,images[0] ))
-            filename, file_extension = os.path.splitext( images[0])
-            if os.path.exists( os.path.join(labels_folder, f"{filename}.txt" )):
-                if not os.path.exists( os.path.join(labels_target_current, f"{filename}.txt" )):
-                    shutil.move (  os.path.join(labels_folder, f"{filename}.txt" ), os.path.join( labels_target_current,f"{filename}.txt" ))
-
-
-            images = images[1:]
-            if not len(images):
-                break
-        
-        if not len(images):
-            break
-        
 
 def packer():
     parser = argparse.ArgumentParser( description='Can split big folders into npacs' )
     parser.add_argument('--imput', "-i", type=dir_path, help="source directory images")
     parser.add_argument('--ilput', "-l", type=dir_path, help="source directory labels")
-    parser.add_argument('--count', "-c", type=str, help="count of object in one subfolder")
+    parser.add_argument('--count', "-c", type=int, help="count of resulting packs")
+    parser.add_argument('--out', "-o", type=str, help="base packs name")
     args = parser.parse_args()
     images_folder = args.imput
     labels_folder = args.ilput
-    countInFolder = int(args.count)
-    main(images_folder, labels_folder, countInFolder)
+    packs_count = int(args.count)
+    packs_name = args.out
+    main(images_folder, labels_folder, packs_count, packs_name)
