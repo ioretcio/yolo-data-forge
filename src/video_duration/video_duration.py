cc351920104f5ae2edc4d843cb711241688722fb
ioretcio <goncharukyou@gmail.com>
2024-03-25 15:05:15 +0200

    recursive dir video dur

--- src/video_duration/video_duration.py
+++ src/video_duration/video_duration.py
@@ -2,26 +2,31 @@ import os
 from moviepy.editor import VideoFileClip
 import argparse
 
+def duration(directory_path):
+    total_duration = 0
+    filecount = 0
 
-def duration():
-    parser = argparse.ArgumentParser(
-        description='This program counts total duration of video in folder.')
-    parser.add_argument('--root_dir', "-d", help="Root directory of deleting")
+    for root, dirs, files in os.walk(directory_path):
+        filecount += len(files)
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            if any(filepath.endswith(ext) for ext in ['.mp4', '.avi', '.mkv', '.mov']):
+                try:
+                    video = VideoFileClip(filepath)
+                    total_duration += video.duration
+                    print(f"duration = {total_duration} seconds")
+                except Exception as e:
+                    print(f"Error processing {filename}: {e}")
+
+    print(f"Total duration of all videos in {directory_path}: {total_duration} seconds")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This program counts the total duration of videos in a folder recursively.')
+    parser.add_argument('--root_dir', "-d", help="Root directory containing videos")
     args = parser.parse_args()
     directory_path = args.root_dir
     
-    total_duration = 0
-    files = os.listdir(directory_path)
-    filecount = len(files)
-    current = 0
-    for filename in os.listdir(directory_path):
-        filepath = os.path.join(directory_path, filename)
-        if os.path.isfile(filepath) and any(filepath.endswith(ext) for ext in ['.mp4', '.avi', '.mkv', '.mov']):
-            try:
-                video = VideoFileClip(filepath)
-                total_duration += video.duration
-                current+=1
-                print(f"duration = {total_duration}, means ~ {total_duration/(current/filecount)}, like a ~ {(total_duration/(current/filecount))/2} frames")
-            except Exception as e:
-                print(f"Error processing {filename}: {e}")
-    print(f"Total duration of all videos in the {directory_path}: {total_duration} seconds")
+    if directory_path:
+        duration(directory_path)
+    else:
+        print("Please provide the root directory using --root_dir argument.")
